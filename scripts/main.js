51bdc363317f753a77a742a7688f762a7b272dd9
SantiagoTonda <superorbe@gmail.com>
2023-03-04 17:32:29 -0300

    fix: styles for cards

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,19 @@ let cardEvents = document.getElementById('allEvents');
 function showCards() {
   for (let element of data.events) {
     let card = `
-      <div>
+      <div class="d-flex flex-column align-items-center justify-content-between">
         <img src= "${element.image}" alt="${element.name}."></img>
           <p class="h3 mt-2">${element.name}</p>
-          <p class="h5">${element.description}</p>
-          <div class="d-flex justify-content-around align-items-center mt-3">
-            <p class="m-0">${element.price}</p>
-            <a href="./details.html" class="btn btn-outline-secondary btn-shadow">Details</a>
+          <p>${element.description}</p>
+          <div class="d-flex align-items-center mb-3">
+            <p class="my-0 me-5">Price: $${element.price}</p>
+            <a href="./details.html" class="btn btn-outline-secondary btn-shadow ms-5">Details</a>
           </div>
       </div> `
 
     allEvents.push(card);
   }
-  cardEvents.innerHTML = allEvents;
+  cardEvents.innerHTML = allEvents.join(" ");
 }
 showCards();
 
