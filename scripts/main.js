19d8caac9d55078db2d36c9238eaf46b8466fa02
SantiagoTonda <superorbe@gmail.com>
2023-03-04 14:00:16 -0300

    add: dynamic cards in index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,44 @@
-let = pastEvents = [];
+let allEvents = [];
+let cardEvents = document.getElementById('allEvents');
+
+function showCards() {
+  for (let element of data.events) {
+    let card = `
+      <div>
+        <img src= "${element.image}" alt="${element.name}."></img>
+          <p class="h3 mt-2">${element.name}</p>
+          <p class="h5">${element.description}</p>
+          <div class="d-flex justify-content-around align-items-center mt-3">
+            <p class="m-0">${element.price}</p>
+            <a href="./details.html" class="btn btn-outline-secondary btn-shadow">Details</a>
+          </div>
+      </div> `
+
+    allEvents.push(card);
+  }
+  cardEvents.innerHTML = allEvents;
+}
+showCards();
+
+
+
+
+
+
+
+
+
+
+/* let = pastEvents = [];
 let = upcomingEvents = [];
 
-for(let each of data.events) {
-  if(each.date < data.currentDate) {
-    pastEvents.push(each)
+for(let element of data.events) {
+  if(element.date < data.currentDate) {
+    pastEvents.push(element)
   } else {
-    upcomingEvents.push(each)
+    upcomingEvents.push(element)
   }
 }
 
 console.log(upcomingEvents);
-console.log(pastEvents);
+console.log(pastEvents); */
