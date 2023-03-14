d877040314fb35237615167d00f4f53d9846b4e6
SantiagoTonda <superorbe@gmail.com>
2023-03-14 14:59:05 -0300

    fix: detail cards

--- scripts/details.js
+++ scripts/details.js
@@ -1,5 +1,10 @@
+/* Falta agregar card de error cuando no exista lo que se busca
+   Falta modificar estilos del details y el stats */
+
+
 let queryString = location.search;
 let urlParams = new URLSearchParams(queryString);
+console.log(urlParams);
 let id = urlParams.get('id') - 1;
 let from = urlParams.get('from');
 if (from == "allEvents") {
@@ -8,21 +13,22 @@ if (from == "allEvents") {
 let evento = data.events[id];
 
 function printDetail() {
-    document.getElementById('details').innerHTML = `<div class="details d-flex flex-column align-items-center flex-md-row">
-    <div class="detail-div-img">
-        <img class="detail-img" src="${evento.image}" alt="${evento.name}.">
+    document.getElementById('details').innerHTML = 
+    `<div class="details d-flex flex-column align-items-center flex-md-row">
+        <div class="detail-div-img">
+            <img class="detail-img" src="${evento.image}" alt="${evento.name}.">
+        </div>
+        <div class="detail-text d-flex flex-column align-items-center justify-content-between">
+            <p class="h1">${evento.name}</p>
+            <p class="h4">${evento.date}</p>
+            <p class="h4">${evento.description}</p>
+            <p class="h4">Category: ${evento.category}</p>
+            <p class="h4">Place: ${evento.place}</p>
+            <p class="h4">Capacity: ${evento.capacity}</p>
+            <p class="h4">Assistance: ${evento.assistance}</p>
+            <p class="h4">Price: $${evento.price}</p>
+        </div>
     </div>
-    <div class="detail-text d-flex flex-column align-items-center justify-content-between">
-        <p class="h1">${evento.name}</p>
-        <p class="h4">${evento.date}</p>
-        <p class="h4">${evento.description}</p>
-        <p class="h4">${evento.category}</p>
-        <p class="h4">${evento.place}</p>
-        <p class="h4">${evento.capacity}</p>
-        <p class="h4">${evento.assistance}</p>
-        <p class="h4">${evento.price}</p>
-    </div>
-</div>
-<a href="./${from}.html" class="btn btn-outline-dark mt-4">Go back</a>`
+    <a href="./${from}.html" class="btn btn-outline-dark mt-4">Go back</a>`
 }
 printDetail();
